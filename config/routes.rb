63c4d4f5c68f65695fd9dab2fa4c3f4345fdecbd
Stephan Fuchs <stephanfuchs@thrivealts.com>
2024-05-03 17:12:44 +0800

    Route tweaks

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,5 @@ Sidekiq::Web.use Rails.application.config.session_store, Rails.application.confi
 Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
   root 'pages#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
   mount Sidekiq::Web => '/sidekiq'
 end
